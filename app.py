33d400a294c746f44e6948c4bcb7dc4990af8a5c
HariharanSV <46956177+Hariharan20@users.noreply.github.com>
2020-04-04 22:33:08 +0530

    app.py added stations route

--- app.py
+++ app.py
@@ -5,28 +5,33 @@ Created on Wed Mar 18 19:04:17 2020
 @author: DELL
 """
 
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,make_response
 from flask import Markup,flash,session,redirect,url_for
-from flask_session import Session
+# from flask_session import Session
 import mysql.connector
 from runenv import load_env
 import os
+import ast
+import json
 load_env()
+
+#config= os.environ.get('config')
 user= os.environ.get('user')
 host= os.environ.get('host')
 passwd= os.environ.get('passwd')
 database= os.environ.get('database')
-config={
-   'user':user,
-   'host':host,
-   'passwd':passwd,
-   'database':database,   
+config = {
+'user':user,
+'host':host,
+'passwd':passwd,
+'database':database
 }
+
 SESSION_TYPE = 'memcache'
 
 app=Flask(__name__)
 app.static_folder='static'
-sess = Session()
+# sess = Session()
 
 
 @app.route('/login')
@@ -83,16 +88,16 @@ def checktable():
       checker=mycursor.fetchone()[0]
       mycursor.close()
       if(checker==password):
+         resp = make_response(render_template('login.html'))
+         # resp.set_cookie('userID', username)
+         session['user_id']=username
+
+         #return resp
          return redirect('/pickup_station')
+         #fvWTyF1pbV
       else:
-         mydb = mysql.connector.connect(**config)
-         mycursor = mydb.cursor()
-         mycursor.execute("SELECT * FROM users")
-         for x in mycursor:
-            print(x)
          return 'Login Failed'
 
-#error?? while running this code... error
 
 @app.route('/start_station',methods=['POST'])
 def stations():
@@ -104,16 +109,36 @@ def stations():
 
 @app.route('/pickup_station')
 def view():
-    return render_template('stations.html')
+   if('user_id' in session):
+      return render_template('stations.html',userid=session['user_id'])
+   else:
+      return redirect('/login')
+
+@app.route('/book')
+def bookbike():
+   result=[["TN72BJ6087","TVS Scooty"],["TN72AM8262","TVS Star"]]
+   return render_template('vehicles.html',table=result)
+
+@app.route('/book_process/<name>', methods=['POST'])
+def displaybookedvehicle(name):
+   if(request.method == "POST"):
+      print("Before")
+      print("Got ",name)
+      return str(name)
+
+@app.route('/ride')
+def ridevehicle():
+   if('station' in session):
+      return render_template('ride.html',station=session['station'])
+   else:
+      return redirect('/login')
 
 @app.errorhandler(404)
 def resource_not_found(e):
    return redirect(url_for('login'))
 
-#now create account
-
 if __name__ == '__main__':
    app.secret_key = os.environ.get('secret_key')
    app.config['SESSION_TYPE'] = 'filesystem'
-   sess.init_app(app)
-   app.run(debug=True)
+   # sess.init_app(app)
+   app.run(debug=True,port=2011)
